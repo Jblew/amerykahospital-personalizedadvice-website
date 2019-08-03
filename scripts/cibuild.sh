931d3cf7c7dc42ec5389795d6b0eb1499398b933
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-08-03 17:18:49 +0200

    ci: run htmlproffer after subservice build

--- scripts/cibuild.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e # fail on error
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../public"
-cd "${DIR}"
-
-bundle exec jekyll build
-bundle exec htmlproofer ./_site
