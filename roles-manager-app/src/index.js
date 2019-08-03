02f2855adce045904f704d6f91a49184a027c5bd
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-08-03 14:24:11 +0200

    feat: setup roles-manager-app

---
+++ roles-manager-app/src/index.js
@@ -0,0 +1,18 @@
+const {
+    FIREBASE_CONFIG,
+    RolesConfig
+} = require("amerykahospital-personalizedadvice-core");
+
+const {
+    FirestoreRolesManagerAppUI
+} = require("firestore-roles-manager-ui");
+
+const config = {
+    title: "AmerykaApp roles management",
+    firebase: FIREBASE_CONFIG,
+    authProviders: ["google.com", "password"],
+    roles: RolesConfig,
+    basePath: window.location.hostname === "localhost" ? "/dist/" : "/role-management/",
+};
+
+FirestoreRolesManagerAppUI.mount("#app", config);
