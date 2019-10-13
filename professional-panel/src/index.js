1733d77dc98954d8ac153348cb57b50c223dfb78
Jędrzej Lewandowski <jedrzej@pitchup.com>
2019-10-13 20:57:03 +0000

    feat: upgrade medicalprofessional app to 1.6.0

--- professional-panel/src/index.js
+++ professional-panel/src/index.js
@@ -1,9 +1,17 @@
 const {
+    FIREBASE_CONFIG,
+    chatConfig
+} = require("../../settings");
+
+const {
     AmerykahospitalPersonalizedadviceMedicalprofessionalappUI
 } = require("amerykahospital-personalizedadvice-medicalprofessional-app");
 
 const config = {
     basePath: window.location.hostname === "localhost" ? "/dist/" : "/professional-panel/",
+    chatConfig: chatConfig,
+    firebase: FIREBASE_CONFIG,
+    evidenceIdHashSalt: FIREBASE_CONFIG.evidenceHashSalt,
 };
 
 AmerykahospitalPersonalizedadviceMedicalprofessionalappUI.mount("#app", config);
